164be9b404a5ed9aa82774a460eb36caae1c9a54
Xmas <findmyxmastree@gmail.com>
2016-04-11 17:34:26 +0000

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,14 @@ Rails.application.routes.draw do
   resources :products, only: [:index]
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
-  root to: "products#index"
+  root to:  "products#index"
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   #root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
